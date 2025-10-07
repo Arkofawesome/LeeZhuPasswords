1485d75094bd777cf7dd6dd52f8c0cdeea7ab8f2
Jackson Lee <jacksonjealee@gmail.com>
2025-10-07 09:21:03 -0400

    changes

--- src/LeeZhuIntFace.java
+++ src/LeeZhuIntFace.java
@@ -1,5 +1,20 @@
 public interface LeeZhuIntFace {
 
+    int MIN_LENGTH = 8;
+
     int determineStrength();
 
+    boolean minRequirements();
+
+    int passLength();
+
+    int conseqIndexes();
+
+    int upperLower();
+
+    int consecutive();
+
+    int appear();
+
+    int recur();
 }
